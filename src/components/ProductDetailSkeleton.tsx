13736c046164004b7f2f220f1be7ef0573759ddb
VivekS <vivek20sv@gmail.com>
2024-10-09 09:02:47 +0530

    ui changes

--- src/components/ProductDetailSkeleton.tsx
+++ src/components/ProductDetailSkeleton.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const ProductDetailSkeleton = () => {
   return (
-    <div className="container mx-auto p-8 mt-2 animate-pulse">
+    <div className="container mx-auto px-2 py-4 mt-2 animate-pulse">
       <div className="flex flex-col md:flex-row">
         <div className="bg-gray-300 h-[70vh] w-full md:w-1/2 rounded-md mb-4 p-4 "></div> 
         <div className="w-1/2 md:ml-8 mt-4 md:mt-0 flex flex-col space-y-4 animate-pulse">

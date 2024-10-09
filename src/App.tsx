10623a0507a6b16085bc6d62ec0fe131a368fd2b
VivekS <vivek20sv@gmail.com>
2024-10-09 07:29:14 +0530

    Update: changes in UI

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ProductDetailPage from './pages/ProductDetailPage';
-
+import './App.css'
 const App: React.FC = () => {
   return (
     <Router>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import CategoryFilter from './CategoryFilter';
 const Header: React.FC = () => {
   return (
     <header className="bg-blue-600 text-white p-6 flex justify-between w-full flex-wrap">
-      <h1 className="text-2xl font-bold mb-4 w-full md:w-6/12 ">Product Dashboard</h1>
+      <h1 className="text-2xl align-middle font-bold w-full md:w-6/12 ">Product Dashboard</h1>
       <div className="flex flex-col sm:flex-row gap-[1rem] md:gap-10 w-full md:w-6/12 h-full">
         <SearchBar />
         <CategoryFilter />
